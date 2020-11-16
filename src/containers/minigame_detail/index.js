989b8eebac9dcc36c9b22def0fc42fdec8b10e02
namhd1992 <stpnghd.vn@gmail.com>
2020-11-16 17:05:52 +0700

    fix

--- src/containers/minigame_detail/index.js
+++ src/containers/minigame_detail/index.js
@@ -38,25 +38,26 @@ class Minigame_detail extends React.Component {
 			link:'',
 			width_ifranme:0,
 			height_iframe:0,
-			horizontal:false
+			horizontal:false,
+			mobile:false,
 		};
 	}
 	componentWillMount(){
 		
 		var id_game=this.props.location.state.id;
-		// var width_ifranme=window.screen.width;
-		// var height_iframe=window.screen.height
+		var width_ifranme=window.screen.width;
+		var height_iframe=window.screen.height
 		// var list_game=[{id:1, img:'https://i.postimg.cc/fWvL01y0/cara.png', name:'Cờ Caro'}, {id:2, img:'https://i.postimg.cc/L5v8KgwV/Flappy-Bird-icon.png', name:'Flappy Bird'}, {id:3, img:'https://i.postimg.cc/8cTz0mK5/snake.png', name:'Rắn Săn Mồi'}, {id:4, img:'https://i.postimg.cc/tCM93bj7/town.jpg', name:'Xếp Tháp'}, {id:5, img:'https://i.postimg.cc/C5NmCyMc/t-i-xu-ng.jpg', name:'Xếp Gạch'}];
 		var list_game=[{id:2, img:'https://i.postimg.cc/L5v8KgwV/Flappy-Bird-icon.png', name:'Flappy Bird', link:'https://www.gamearter.com/game/flappy-superhero-dunk/',horizontal:true}, {id:3, img:'https://i.postimg.cc/8cTz0mK5/snake.png', name:'Rắn Săn Mồi', link:'https://html5.gamedistribution.com/54ca781032914740a819c2242b770878/?gdpr-targeting=1', horizontal:true}, {id:4, img:'https://i.postimg.cc/tCM93bj7/town.jpg', name:'Xếp Tháp', link:'https://html5.gamedistribution.com/rvvASMiM/e49e377096194ed383dba2a18a110498/?gd_zone_config=eyJwYXJlbnRVUkwiOiJodHRwczovL2tpejEwLmNvbS8iLCJwYXJlbnREb21haW4iOiJraXoxMC5jb20iLCJ0b3BEb21haW4iOiJraXoxMC5jb20iLCJoYXNJbXByZXNzaW9uIjp0cnVlLCJsb2FkZXJFbmFibGVkIjp0cnVlLCJ2ZXJzaW9uIjoiMS4xLjM5In0%253D', horizontal:false}, {id:5, img:'https://i.postimg.cc/C5NmCyMc/t-i-xu-ng.jpg', name:'Xếp Gạch', link:'https://tetris.fbrq.io/tetris/index.html', horizontal:false}];
 		var pos = list_game.map(function(e) { return e.id; }).indexOf(id_game);
 		if(isMobile){
-			
+			this.setState({list_game:list_game, link:list_game[pos].link, width_ifranme:width_ifranme, height_iframe:height_iframe, mobile:true})
 		}else{
 			var horizontal=list_game[pos].horizontal;
 			if(horizontal){
-				this.setState({list_game:list_game, link:list_game[pos].link, width_ifranme:768, height_iframe:500})
+				this.setState({list_game:list_game, link:list_game[pos].link, width_ifranme:768, height_iframe:500, mobile:false, horizontal:horizontal})
 			}else{
-				this.setState({list_game:list_game, link:list_game[pos].link, width_ifranme:400, height_iframe:600})
+				this.setState({list_game:list_game, link:list_game[pos].link, width_ifranme:400, height_iframe:600, mobile:false, horizontal:horizontal})
 			}
 		}
 		// this.setState({list_game:list_game, link:list_game[pos].link, width_ifranme:width_ifranme, height_iframe:height_iframe})
@@ -82,7 +83,12 @@ class Minigame_detail extends React.Component {
 	}
 
 	render() {
-		const {list_game, id_game, link, width_ifranme, height_iframe}=this.state;
+		const {list_game, id_game, link, width_ifranme, height_iframe, mobile,horizontal}=this.state;
+		var mgl=0;
+		if(!horizontal){
+			mgl=200;
+			console.log('AAAAAAAA')
+		}
 
 		// if (id_game===1){
 		// 	return (
@@ -111,8 +117,8 @@ class Minigame_detail extends React.Component {
 		// 	)
 		// }
 
-		return(
-			<div class="container" style={{paddingLeft:200}}>
+		return(<div>
+			{(mobile)?(<div>
 				<iframe id='game' width={width_ifranme} height={height_iframe} frameBorder='0' allow='autoplay' allowTransparency="true" allowFullScreen="true" seamless scrolling='no' src={link}></iframe>	
 				<div class="modal fade" id="ads" style={{
                                             backgroundImage: "url('https://i.postimg.cc/2SL8rjPm/truykich.png')",
@@ -130,10 +136,12 @@ class Minigame_detail extends React.Component {
 						</div>
 					</div>
 				</div>
-			</div>
-		)
-		
-		
+
+			</div>):(<div  class="container" style={{paddingLeft:mgl}}>
+				<iframe id='game' width={width_ifranme} height={height_iframe} frameBorder='0' allow='autoplay' allowTransparency="true" allowFullScreen="true" seamless scrolling='no' src={link}></iframe>	
+			</div>)}
+				
+		</div>)
 	}
 }
 
